9935efb72811945cb6b8724764d49539181b476e
DELL <DELL@DESKTOP-48PMLUL.intelbras.local>
2021-07-26 14:27:55 -0300

    Combate

---
+++ aula07/src/aula07/Estudo07.java
@@ -0,0 +1,33 @@
+package aula07;
+
+public class Estudo07 {
+
+	public static void main(String[] args) {
+		
+
+		
+	Lutador l[] = new Lutador [6]; 
+	
+	
+	l[0] = new Lutador ("Pretty boy", "França", 31, 1.75, 68.9, 11, 3, 1);
+	
+	l[1] = new Lutador ("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+	
+	l[2] = new Lutador ("Snapchadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+	
+	l[3] = new Lutador ("Dead code", "Australia", 28, 1.93, 81.6, 13 , 0, 2);
+
+	l[4] = new Lutador ("Ufocobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+			
+	l[5] = new Lutador ("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+	
+
+	
+	Luta UEC01 = new Luta ();
+	UEC01.marcarLuta(l[0], l[1]);
+	UEC01.lutar();
+
+	
+	}
+
+}
